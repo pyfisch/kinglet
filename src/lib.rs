de498f282dcaf275e4b32c7142d02f5958773f97
Pyfisch <pyfisch@gmail.com>
2017-01-04 17:21:43 +0100

    Kinglet NG now using tokio. Complete rewrite.

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,13 @@
 extern crate httparse;
-extern crate hyper;
-extern crate mio;
-extern crate netbuf;
-extern crate rotor;
-extern crate unicase;
-extern crate url;
-extern crate time;
-extern crate multimap;
+extern crate httpdate;
+extern crate httptypes;
+extern crate tokio_core;
+extern crate tokio_proto;
+extern crate tokio_service;
 
-use rotor::transports::{accept, stream};
-
-pub use hyper::method::Method;
-pub use hyper::status::StatusCode;
-pub use hyper::version::HttpVersion;
-pub use mio::{EventLoop};
-pub use mio::tcp::{TcpListener, TcpStream};
-pub use rotor::Handler as EventHandler;
-pub use url::Url;
-
-pub use error::{Error, Result};
-pub use headers::{IterListHeader, Headers};
-pub use http1::Handler;
-pub use message::Message;
-pub use request::Request;
-pub use response::Response;
-
-mod error;
-mod headers;
-pub mod http1;
-mod message;
-mod request;
-mod response;
-
-pub type HttpServer<C, R> = accept::Serve<C,
-                        TcpListener,
-                        stream::Stream<C, TcpStream, http1::Client<C, R>>>;
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
